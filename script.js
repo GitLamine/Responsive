662165c5f48a09da061b01880182db2a9a8cec18
LamCode47 <mohamedlamine.sialarbi@gmail.com>
2023-05-31 16:36:55 +0200

    correction error404

--- script.js
+++ script.js
@@ -3,21 +3,20 @@ let input = document.querySelector("#codeP");
 
 input.addEventListener('input', function () {
 
-    
-    if (this.value.length == 5){
-    let url = `https://geo.api.gouv.fr/communes?codePostal=${this.value}&type=commune-actuelle&fields=nom,code,codesPostaux&format=json&geometry=centre`;
 
-    fetch(url) 
-    .then(response => response.json())
-    .then(data => {
-        console.dir(data);
-        let list = "<ul>";
-        for (let ville of data){
-            list += `<li>${ville.nom}</li>`
-        }
-        list +='</ul>';
-        document.querySelector(".ville").innerHTML = list
-    });
+    if (this.value.length == 5) {
+        let url = `https://geo.api.gouv.fr/communes?codePostal=${this.value}&type=commune-actuelle&fields=nom,code,codesPostaux&format=json&geometry=centre`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                let list = "<ul>";
+                for (let ville of data) {
+                    list += `<li>${ville.nom}</li>`
+                }
+                list += '</ul>';
+                document.querySelector(".ville").innerHTML = list
+            });
     }
 });
 
